379b43fcaacf1b00e7848fe7e4275af43a75607a
sujal ahuja <sujalahuja.sa@gmail.com>
2025-12-19 17:13:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./sections/Hero.jsx/Hero";
+import Header from "./sections/Header/Header";
+const App = () => {
+  return (
+    <main>
+      <Header />
+      {/* <Hero /> */}
+    </main>
+  );
+};
+
+export default App;
---
+++ src/sections/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "@assets/psychoart.svg";
+import search from "@assets/search.svg";
+
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="container">
+      <nav className="header">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="search-bar">
+          <img src={search} alt="" />
+          <input type="text" placeholder="Search items and collections" />
+        </div>
+        <div className="nav-ul">
+          <ul>
+            <li className="nav-link">
+              <a href="">Collections</a>
+            </li>
+            <li className="nav-link">
+              <a href="">Features</a>
+            </li>
+            <li className="nav-link">
+              <a href="">FAQ</a>
+            </li>
+            <button className="header-btn">Select Wallet</button>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
