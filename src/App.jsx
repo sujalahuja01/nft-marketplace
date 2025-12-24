e080870bc7ff0e9fd522f345bb9223459a9fbba2
sujal ahuja <sujalahuja.sa@gmail.com>
2025-12-24 10:31:32 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./sections/Header/Header";
 import Sponsers from "./sections/Sponsers/Sponsers";
 import About from "./sections/About/About";
 import Collections from "./sections/Collections/Collections";
-import Features from "./sections/Features/features";
+import Features from "./sections/Features/Features";
 import Creators from "./sections/Creators/Creators";
 import FAQ from "./sections/FAQ/FAQ";
 import Contact from "./sections/Contact/Contact";
